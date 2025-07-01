13c3435f5fbfddd03574c1f7bd150ac08a2b70ef
Tushar <tusharrathory60@gmail.com>
2025-07-01 10:50:42 +0530

    buffer added

---
+++ ai-studypal/public/js/script.js
@@ -0,0 +1,27 @@
+
+  window.addEventListener("DOMContentLoaded", () => {
+    const response = document.getElementById("response-section");
+
+    if (response) {
+      setTimeout(() => {
+        response.scrollIntoView({
+          behavior: "smooth",
+          block: "start"
+        });
+      }, 300);
+    }
+    const profileImage = document.getElementById("profile-pic");
+    if (profileImage) {
+      profileImage.addEventListener("click", () => {
+        alert("Coming soon!");
+      });
+    }
+    const form = document.querySelector("form");
+    const spinner = document.getElementById("loading-spinner");
+
+    if (form && spinner) {
+      form.addEventListener("submit", () => {
+        spinner.style.display = "block";
+      });
+    }
+  });
